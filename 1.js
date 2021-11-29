5d28240ff052e5a3a52a593755614489e0e5c29c
lmonsalve22 <lmonsalve22@gmail.com>
2021-11-29 19:17:45 -0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,49 @@
+puntos = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -72.66731651115752,
+                    -38.03749999657841
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13404",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "Paine",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "TENENCIA CARRETERAS - TCIA. CARRET. PAINE",
+                "POINT_X": -70.735334213,
+                "POINT_Y": -33.89377388
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -72.67282119232824,
+                    -38.48333332987164
+                ]
+            },
+            "properties": {
+                "CUT_REG": "13",
+                "CUT_PROV": "134",
+                "CUT_COM": "13401",
+                "REGION": "Metropolitana de Santiago",
+                "PROVINCIA": "Maipo",
+                "COMUNA": "San Bernardo",
+                "variable": "Estaci\u00f3n de Carabineros",
+                "Nombres": "COMISARIA - 14A. COM. SAN BERNARDO",
+                "POINT_X": -70.701290222,
+                "POINT_Y": -33.5921081856
+            }
+        }
+    ]
+}
