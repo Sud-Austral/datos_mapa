9c152c19c30969c93358fcc699abc4679734ab44
lmonsalve22 <lmonsalve22@gmail.com>
2021-11-30 12:35:28 -0300

    Update Servidor.py

--- Servidor.py
+++ Servidor.py
@@ -7,11 +7,9 @@ import pandas as pd
 if __name__ == '__main__':
     print('Iniciado proceso...')
     #service_account = 'foo-project@appspot.gserviceaccount.com'
-    #credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
-    #ee.Initialize(credentials)
-    from google.auth import compute_engine
-    import ee
-    credentials = compute_engine.Credentials(scopes=['https://www.googleapis.com/auth/earthengine'])
+    service_account = '722158313283-gem3r97no94a7fgdchjaj11qanhv3i2p.apps.googleusercontent.com'    
+    credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
     ee.Initialize(credentials)
+
     studyArea = ee.FeatureCollection("users/APP_DATA_I/limites/CHL_regiones_sim")
     print('Proceso finalizado.')
