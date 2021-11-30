47f4a90533c4f4d5decc911943792eaea21218f0
lmonsalve22 <lmonsalve22@gmail.com>
2021-11-30 17:07:11 -0300

    asd

--- Servidor.py
+++ Servidor.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     print('Iniciado proceso...')
     service_account = 'foo-project@appspot.gserviceaccount.com'
     #service_account = '722158313283-gem3r97no94a7fgdchjaj11qanhv3i2p.apps.googleusercontent.com'    
-    credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
+    credentials = ee.ServiceAccountCredentials(service_account, 's3.json')
     ee.Initialize(credentials)
 
     studyArea = ee.FeatureCollection("users/APP_DATA_I/limites/CHL_regiones_sim")
