b4bfa29ff0327130ea7c79f5a683d924887536ab
lmonsalve22 <lmonsalve22@gmail.com>
2021-11-30 12:48:36 -0300

    asd

--- Servidor.py
+++ Servidor.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 if __name__ == '__main__':
     print('Iniciado proceso...')
-    #service_account = 'foo-project@appspot.gserviceaccount.com'
-    service_account = '722158313283-gem3r97no94a7fgdchjaj11qanhv3i2p.apps.googleusercontent.com'    
+    service_account = 'foo-project@appspot.gserviceaccount.com'
+    #service_account = '722158313283-gem3r97no94a7fgdchjaj11qanhv3i2p.apps.googleusercontent.com'    
     credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
     ee.Initialize(credentials)
 
