cdd3455dd9f189b117755fd05bb5e9a0aadbe1eb
lmonsalve22 <lmonsalve22@gmail.com>
2021-11-30 12:25:51 -0300

    asd

--- Servidor.py
+++ Servidor.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 if __name__ == '__main__':
     print('Iniciado proceso...')
-    ee.Initialize()
+    service_account = 'foo-project@appspot.gserviceaccount.com'
+    credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
+    ee.Initialize(credentials)
     studyArea = ee.FeatureCollection("users/APP_DATA_I/limites/CHL_regiones_sim")
     print('Proceso finalizado.')
